3db493e37b584d09703a2d26da740e85c8420851
Mohammednur <mamebb2023@gmail.com>
2023-01-24 15:21:06 +0300

    Finds the right functions to use

--- _printf.c
+++ _printf.c
@@ -9,19 +9,14 @@ int _printf(const char * const format, ...)
 {
 	convert_match m[] = {
 		{"%s", printf_string},
-		{"%c", printf_char},
-		{"%%", printf_37},
+		{"%c", printf_char}, {"%%", printf_37},
 		{"%i", printf_int},
-		{"%d", printf_dec},
-		{"%r", printf_srev},
+		{"%d", printf_dec}, {"%r", printf_srev},
 		{"%R", printf_rot13},
-		{"%b", printf_bin},
-		{"%u", printf_unsigned},
+		{"%b", printf_bin}, {"%u", printf_unsigned},
 		{"%o", printf_oct},
-		{"%x", printf_hex},
-		{"%X", printf_HEX},
-		{"%S", printf_exclusive_string},
-		{"%p", printf_pointer}
+		{"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
 	};
 
 	va_list args;
@@ -37,7 +32,8 @@ Here:
 		j = 13;
 		while (j >= 0)
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			if (m[j].id[0] == format[i] &&
+				       m[j].id[1] == format[i + 1])
 			{
 				len += m[j].f(args);
 				i = i + 2;
